32598a0985c9fa80838cffab4309a958526d16f3
Mukilan-T <mukilanthangadurai@gmail.com>
2021-11-27 17:33:35 +0530

    Patch

--- welcome_git.py
+++ welcome_git.py
@@ -1,2 +1,5 @@
 print("Git is a free and distributed open source system!")
 print("designed to handle everything from small to very large projects with speed and efficiency.")
+import array as arr
+a=arr.array('i',[1,3,5,7,9])
+print(a)
